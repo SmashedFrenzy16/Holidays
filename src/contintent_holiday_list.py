4378932effb3dc470ecac1f48491d4f6b5cabfc3
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2023-05-14 20:21:38 +0100

    Update contintent_holiday_list.py

--- src/contintent_holiday_list.py
+++ src/contintent_holiday_list.py
@@ -3,6 +3,6 @@ from holidays import *
 
 year = int(input("Enter in the year that you want holidays from: "))
 
-north_america = CA(years=year) + US(years=year) + MX(years=year)
+north_america = CA(years=year) + US(years=year) + MX(years=year) # Replace The continent with your own continent codes
 
 print(north_america)
